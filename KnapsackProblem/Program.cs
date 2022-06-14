bdee6365a992f66cd6c739337a005d454cb4b605
Krysia <krystynaszybalska@skaleo.pl>
2022-06-14 12:48:36 +0200

    added changes in time to output

--- KnapsackProblem/Program.cs
+++ KnapsackProblem/Program.cs
@@ -1,17 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace KnapsackProblem
 {
     class Program
     {
-        //objetosc 2,5tys jednostek
-            //100 przedmiotow o objetosci 1-10 naturalne
-            //objetosc - ma zostac jak najmniej
-            //ewolucyjnie - rodzic i dziecko, 1,0, albo bierzemy albo nie
-            //operacja mutacji
-            //lepsze lub przepełnione idealne
-            //oba przepełnione - mniejsze przepełnienie
-            //wydrukować przedmioty wzięte do plecaka i wolne miejsce
         static void Main(string[] args)
         {
             int knapsackVolume = 2500;
@@ -35,29 +28,31 @@ namespace KnapsackProblem
                 child[i] = parent[i];
             }
 
-            int emptySpaceLeftParent=0;
+            int emptySpaceLeftParent=CalculateEmptySpace(items, parent, knapsackVolume);
+            int emptySpaceLeftChild = emptySpaceLeftParent;
+            
             bool perfect = false;
             int failureCounter = 0;
+            List<int> emptySpace = new List<int>();
 
-            while (!perfect || failureCounter>10)
+            while (!perfect && failureCounter<1000)
             {
-                emptySpaceLeftParent = CalculateEmptySpace(items, parent, knapsackVolume);
-
                 //change one number in child
                 int toChange = rnd.Next(100);
-                if (child[toChange] == 1) child[toChange] = 0; else child[toChange] = 1;
-                int emptySpaceLeftChild = CalculateEmptySpace(items, child, knapsackVolume);
-
+                if (child[toChange] == 0) child[toChange] = 1; else child[toChange] = 0;
+                emptySpaceLeftChild = CalculateEmptySpace(items, child, knapsackVolume);
+                
                 //if the child is better it becomes a parent
                 switch (Math.Sign(emptySpaceLeftChild), Math.Sign(emptySpaceLeftParent))
                 {
                     case (1, 1):
-                        if (emptySpaceLeftParent > emptySpaceLeftChild)
+                        if (emptySpaceLeftParent >= emptySpaceLeftChild)
                         {
                             parent = child;
                             emptySpaceLeftParent = emptySpaceLeftChild;
                             failureCounter = 0;
                         }
+
                         break;
                     case (-1, -1):
                         if (emptySpaceLeftParent < emptySpaceLeftChild)
@@ -77,14 +72,13 @@ namespace KnapsackProblem
                         break;
                     case (-1, 0):
                     case (1, 0):
-                        perfect = true;
                         failureCounter++;
+                        perfect = true;
                         break;
                     case (0, 1):
                     case (0, -1):
                         parent = child;
                         emptySpaceLeftParent = emptySpaceLeftChild;
-                        failureCounter = 0;
                         perfect = true;
                         break;
                     case (0, 0):
@@ -94,8 +88,11 @@ namespace KnapsackProblem
                     default:
                         break;
                 }
+                
+                emptySpace.Add(emptySpaceLeftParent);
             }
-            //print the answer
+
+            Console.WriteLine();
             Console.WriteLine("Do plecaka zabrano przedmioty nr: ");
             int sum = 0;
             for (int i = 0; i < parent.Length; i++)
@@ -104,9 +101,16 @@ namespace KnapsackProblem
                 {
                     Console.Write(i + ", ");
                 }
-                if (i == parent.Length - 1) Console.Write('\n');
             }
-            Console.Write("pozostała wolna przestrzeń: " + emptySpaceLeftParent);
+            Console.WriteLine("pozostała wolna przestrzeń: " + emptySpaceLeftParent);
+
+            Console.WriteLine();
+            Console.WriteLine("Zmiany pozostałej wolnej przestrzeni z interacji na iterację: ");
+            for (int i = 0; i < emptySpace.Count; i++)
+            {
+                if (i == emptySpace.Count-1) Console.Write(emptySpace[i]);
+                else Console.Write(emptySpace[i] + " >> ");
+            }
         }
 
         private static int CalculateEmptySpace(int[] items, int[] parent, int knapsackVolume )
@@ -120,3 +124,4 @@ namespace KnapsackProblem
         }
     }
 }
+
